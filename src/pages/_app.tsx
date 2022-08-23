8910b0c4fa2cae7e2d04d5e0905c31b83d6359c5
vitordiniz25 <vitordiniz25@gmail.com>
2022-08-23 12:28:05 -0300

    api routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import { AppProps } from "../../node_modules/next/app";
 import "../../styles/global.scss";
 import { Header } from "../components/Header";
+import { Provider as NextAuthProvider } from "next-auth/client";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
+      <NextAuthProvider session={pageProps.session}>
+        <Header />
+        <Component {...pageProps} />
+      </NextAuthProvider>
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home({ product }: HomeProps) {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for {product.amount} month</span>
+            <span>for {product.amount}/month</span>
           </p>
           <SubscribeButton priceId={product.priceId} />
         </section>
