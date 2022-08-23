687172550722c513467ec07377aca6acef8e3fdd
vitordiniz25 <vitordiniz25@gmail.com>
2022-08-22 21:09:51 -0300

    fazendo home page e estilizando

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
 import Head from "next/head";
+import Styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello World</h1>
+
+      <main className={Styles.contentContainer}>
+        <section className={Styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>
+            News about the <span>React</span> world.{" "}
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   );
 }
