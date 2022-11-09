811b0389b0f1709a9b1259f3e8c9bfc519bfeaae
vitordiniz25 <vitordiniz25@gmail.com>
2022-11-09 18:26:12 -0300

    back no front

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
+import { fauna } from "../../../services/fauna";
+import { query as q} from "faunadb";
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -15,4 +18,39 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile }): Promise<boolean> {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user-by-email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              {
+                data: {email}
+              }
+            ),
+                q.Get(
+                  q.Match(
+                    q.Index('user-by-email'),
+                    q.Casefold(user.email)
+                  )
+                )
+              )  
+            );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
