da8bbfe1132a258b2f7ef19b8b2767877df47d20
vitordiniz25 <vitordiniz25@gmail.com>
2023-02-02 22:36:57 -0300

    projeto finalizado

--- src/tests/pages/Posts.spec.tsx
+++ src/tests/pages/Posts.spec.tsx
@@ -1,46 +1,46 @@
 import { render, screen } from "@testing-library/react";
 import { mocked } from "jest-mock";
-import Posts, { getStaticProps } from "../../pages/posts";
 
 import { getPrismicClient } from "../../services/prismic";
+import Posts, { getStaticProps } from "../../pages/posts";
+
+jest.mock("../../services/prismic");
 
 const posts = [
   {
     slug: "my-new-post",
-    title: "my-new-post",
-    excerpt: "Post-excerpet",
-    updatedAt: "02 de fevereiro",
+    title: "My New Post",
+    excerpt: "Post excerpt",
+    updatedAt: "10 de abril",
   },
 ];
-
-jest.mock("../../services/prismic.ts");
 
 describe("Posts page", () => {
   it("renders correctly", () => {
     render(<Posts posts={posts} />);
 
-    expect(screen.getByText("my-new-post")).toBeInTheDocument();
+    expect(screen.getByText("My New Post")).toBeInTheDocument();
   });
 
-  it("load initial data", async () => {
-    const getPrismicMocked = mocked(getPrismicClient);
+  it("loads initial data", async () => {
+    const getPrismicClientMocked = mocked(getPrismicClient);
 
-    getPrismicMocked.mockRejectedValueOnce({
+    getPrismicClientMocked.mockReturnValueOnce({
       query: jest.fn().mockResolvedValueOnce({
         results: [
           {
             uid: "my-new-post",
             data: {
-              title: [{ type: "heading", text: "My new post" }],
+              title: [{ type: "heading", text: "My New Post" }],
               content: [{ type: "paragraph", text: "Post excerpt" }],
             },
-            last_publication_date: "02-02-2023",
+            last_publication_date: "04-01-2021",
           },
         ],
       }),
-    } as never);
+    } as any);
 
-    const response = getStaticProps({});
+    const response = await getStaticProps({});
 
     expect(response).toEqual(
       expect.objectContaining({
@@ -48,9 +48,9 @@ describe("Posts page", () => {
           posts: [
             {
               slug: "my-new-post",
-              title: "my-new-post",
-              excerpt: "excerpt",
-              updatedAt: "02 de feveiro de 2023",
+              title: "My New Post",
+              excerpt: "Post excerpt",
+              updatedAt: "01 de abril de 2021",
             },
           ],
         },
